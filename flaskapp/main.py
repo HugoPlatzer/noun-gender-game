3e7d1621069c35ee3aafa8a34ac0871b3315c087
Hugo Platzer <hugoplatzer@aon.at>
2021-02-17 15:48:59 +0100

    working game, report, login

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -1,25 +1,131 @@
-from flask import Flask, jsonify, redirect, send_from_directory
+from flask import Flask, jsonify, request, redirect, send_from_directory, render_template
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from random import choice
 from pyphen import Pyphen
+import sqlite3
+from datetime import datetime
+import json
 
-app = Flask(__name__,
-    static_url_path="/static",
-    static_folder="static")
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
-wordlist_file = "res/wordlist.txt"
-words = [l.split() for l in open(wordlist_file).readlines()]
-article_choices = ["der", "die", "das"]
-hyphen_dic = Pyphen(lang="de_DE")
+def db_init():
+    db = sqlite3.connect("main.db")
+    db_c = db.cursor()
+    db_c.execute("CREATE TABLE IF NOT EXISTS games ("
+                "game_id TEXT PRIMARY KEY,"
+                "date TEXT NOT NULL,"
+                "num_correct INTEGER NOT NULL,"
+                "num_total INTEGER NOT NULL"
+                ");")
+    db_c.execute("CREATE TABLE IF NOT EXISTS users ("
+                "username TEXT NOT NULL,"
+                "displayname TEXT NOT NULL,"
+                "hash TEXT NOT NULL"
+                ");")
+    db_c.execute("CREATE TABLE IF NOT EXISTS config ("
+                "property TEXT NOT NULL,"
+                "value TEXT NOT NULL"
+                ");")
+    db.commit()
+    return db, db_c
+
 
 def hyphenate(word):
     return hyphen_dic.inserted(word, hyphen="&shy;")
 
+
+def load_config(prop):
+    db_c.execute("SELECT value FROM config WHERE property=?", [prop])
+    data = db_c.fetchone()
+    return data[0]
+
+
+class User:
+    def __init__(self, username, displayname, pwd_hash):
+        self.username = username
+        self.displayname = displayname
+        self.pwd_hash = pwd_hash
+        self.is_authenticated = True
+        self.is_active = True
+        self.is_anonymous = False
+
+    def check_password(self, password):
+        return check_password_hash(self.pwd_hash, password)
+    
+    def get_id(self):
+        return self.username
+
+db, db_c = db_init()
+app = Flask(__name__)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+app.secret_key = load_config("secret_key")
+login_manager = LoginManager(app)
+
+game_nwords = 3
+wordlist_file = "res/wordlists/de_corr.txt"
+words = [l.split() for l in open(wordlist_file).readlines()]
+article_choices = ["der", "die", "das"]
+hyphen_dic = Pyphen(lang="de_DE")
+
+
+@login_manager.user_loader
+def load_user(username):
+    db_c.execute("SELECT username,displayname,hash from users WHERE username=?", [username])
+    data = db_c.fetchone()
+    if data is None:
+        return None
+    return User(data[0], data[1], data[2])
+
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    return redirect("/login")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def page_login():
+    if current_user.is_authenticated:
+        return redirect("/")
+    if request.method == "GET":
+        return send_from_directory("static", "login.html")
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = load_user(username)
+        if user is not None and user.check_password(password):
+            login_user(user, remember=True)
+            return redirect("/")
+        else:
+            return redirect("/login")
+
+
+@app.route("/logout")
+@login_required
+def page_logout():
+    logout_user()
+    return redirect("/login")
+
+
 @app.route("/")
+@login_required
+def page_root():
+    return redirect("/main")
+
+
+@app.route("/main")
+@login_required
 def page_main():
-    return redirect("/static/game.html")
+    return render_template("main.html")
+
+
+@app.route("/game")
+@login_required
+def page_game():
+    return send_from_directory("static", "game.html")
+
 
 @app.route("/word")
+@login_required
 def service_word():
     article, noun = choice(words)
     return jsonify({"article_choices" : article_choices,
@@ -27,3 +133,60 @@ def service_word():
                     "noun" : noun,
                     "noun_hyphen" : hyphenate(noun)
                     })
+
+
+@app.route("/word-result", methods=["POST"])
+@login_required
+def service_word_result():
+    game_id = request.json["gameId"]
+    is_correct = request.json["correct"]
+    db_c.execute("SELECT num_correct, num_total from games WHERE game_id=?", [game_id])
+    data = db_c.fetchone()
+    if data is None:
+        old_correct = 0
+        old_total =  0
+        date = datetime.now().isoformat()
+        db_c.execute("INSERT INTO games VALUES (?, ?, ?, ?)", [game_id, date, 0, 0])
+    else:
+        old_correct = data[0]
+        old_total = data[1]
+    if is_correct:
+        new_correct = old_correct + 1
+    else:
+        new_correct = old_correct
+    new_total = old_total + 1
+    db_c.execute("UPDATE games SET num_correct=?,num_total=? WHERE game_id=?", [new_correct, new_total, game_id])
+    db.commit()
+    return jsonify(success=True)
+
+
+@app.route("/report/<game_id>", methods=["POST"])
+@login_required
+def service_report(game_id):
+    answers = json.loads(request.form.get("data"))
+    
+    db_c.execute("SELECT num_correct, num_total from games WHERE game_id=?", [game_id])
+    data = db_c.fetchone()
+    if data is None:
+        num_correct, num_total = 0, 0
+    else:
+        num_correct, num_total = data[0], data[1]
+    message = "{}/{} articles correct".format(num_correct, num_total)
+    if num_correct != num_total:
+        submessage = "Mistakes:"
+    elif num_total < game_nwords:
+        submessage = "Game aborted"
+    else:
+        submessage = "Good job!"
+    
+    mistakes = []
+    for answer in answers:
+        if answer["guessedArticle"] != answer["correctArticle"]:
+            mistakes.append({"article": answer["correctArticle"], "word": answer["noun_hyphen"]})
+    
+    return render_template("report.html",
+        message=message,
+        submessage=submessage,
+        has_mistakes=(len(mistakes) > 0),
+        mistakes=mistakes)
+    
--- flaskapp/static/game.js
+++ flaskapp/static/game.js
@@ -1,36 +1,70 @@
-var uniqId = (function(){
-    var i=0;
-    return function() {
-        return i++;
-    }
-})();
+function uniqId() {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return Math.random().toString(26).substr(2, 9);
+};
 
 function init_ui() {
-    fetch("/word")
-        .then(response => response.json())
-        .then(data => init_ui_part2(data));
+    toggle_progress_confirm(false);
+    // wait a little before updating the progress bar
+    // if this is not done, after a wrong word the progress bar change will not be animated
+    setTimeout(update_progressbar, 10);
+    
+    if (words_done == words_total) {
+        $(".cancel-button").addClass("invisible");
+        setTimeout(exit_game, 1000);
+    } else {
+        fetch("/word")
+            .then(response => response.json())
+            .then(data => init_ui_part2(data));
+    }
+}
+
+function exit_game() {
+    var report_url = "/report/" + game_id;
+    $.redirect(report_url, {"data": JSON.stringify(answers_list)});
+}
+
+function confirm_exit_game() {
+    
+}
+
+function toggle_progress_confirm(confirm) {
+    if (confirm) {
+        $(".progress-bar-row").addClass("d-none");
+        $(".confirm-button-row").removeClass("d-none");
+    } else {
+        $(".progress-bar-row").removeClass("d-none");
+        $(".confirm-button-row").addClass("d-none");
+    }
+}
+
+function update_progressbar() {
+    var percentage = Math.round(100 * words_done / words_total);
+    $(".progress-bar").css("width", percentage.toString() + "%");
 }
 
 function init_ui_part2(data_json) {
+    data_json_stored = data_json;
+    
     button_event_dict = {};
-    var article_buttons_row = $(".article-buttons-row");
+    var article_buttons_row = $(".article-button-row");
     article_buttons_row.empty();
     
     data_json.article_choices.forEach(function(article) {
-        var button = $("<button class=\"btn btn-primary btn-lg btn-block article-button\">der</button>");
+        var button = $("<button class=\"btn btn-lg m-2 flex-grow-1 btn-primary\"></button>");
         var button_id = uniqId();
         button.html(article);
         button.click(function() {
             article_button_clicked(data_json, button_id);
         });
-        var button_div = $("<div class=\"col-md m-2\">");
-        button_div.append(button);
-        article_buttons_row.append(button_div);
+        article_buttons_row.append(button);
         button_event_dict[button_id] = button;
     });
     
-    var word_container = $(".word-display");
-    word_container.html(data_json.noun_hyphen);
+    var word_row = $(".word-row");
+    word_row.html(data_json.noun_hyphen);
 }
 
 function article_button_clicked(data_json, button_id) {
@@ -38,6 +72,8 @@ function article_button_clicked(data_json, button_id) {
     if (my_button === undefined) { // button no longer active
         return;
     }
+    
+    words_done++;
     var my_article = my_button.html();
     
     var correct_article = data_json.article;
@@ -53,29 +89,51 @@ function article_button_clicked(data_json, button_id) {
         }
     });
     
-    if (my_article === correct_article) { // guessed article correct
+    var report_item = {"noun" : data_json.noun, "noun_hyphen" : data_json.noun_hyphen,
+            "correctArticle" : correct_article,  "guessedArticle" : my_article};
+    answers_list.push(report_item);
+    
+    var word_result = {"gameId" : game_id, "correct" : (my_article === correct_article)};
+    fetch("/word-result", {"method" : "post", "headers" : {"Content-Type": "application/json"}, "body": JSON.stringify(word_result)});
+    
+    if (my_article === correct_article) { // guessed article is correct
         audio_success.play();
-    } else { // guessed article wrong
+        // fetch new word and reset automatically
+        if (words_done == words_total) {
+            init_ui();
+        } else {
+            setTimeout(init_ui, 1000);
+        }
+    } else { // guessed article is wrong
         my_button.removeClass("btn-primary");
         my_button.addClass("btn-danger");
         audio_error.play();
+        // fetch new word after manual confirmation
+        toggle_progress_confirm(true);
     }
-    //~ button.removeClass("btn-primary");
-    //~ button.addClass("btn-success");
-    //~ button.css("pointer-events", "none");
-    //~ button.blur();
     
     button_event_dict = {}; // prevent events for other buttons
-    
-    setTimeout(init_ui, 3000); // fetch new word and reset, after some delay
 }
 
 var button_event_dict = {};
 var audio_success;
 var audio_error;
+var words_done = 0;
+var words_total = 3;
+var answers_list = [];
+var game_id;
+var data_json_stored;
 
 $(document).ready(function() {
-    audio_success = new Audio("sounds/success.mp3");
-    audio_error = new Audio("sounds/error.mp3");
+    audio_success = new Audio("/static/sounds/success.mp3");
+    audio_error = new Audio("/static/sounds/error.mp3");
+    game_id = uniqId();
+    console.log("game id: " + game_id);
+    
+    // should only register this once for the persistent button to prevent multiple inits!
+    $(".confirm-button").click(function() {
+        init_ui();
+    });
+    
     init_ui();
 });
